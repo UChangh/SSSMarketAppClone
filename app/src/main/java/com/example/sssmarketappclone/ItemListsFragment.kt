45781228cf31748fbb3e993663c4d284d9b3d3a5
Hong <151532167+UChangh@users.noreply.github.com>
2024-01-09 21:18:36 +0900

    add : Notification

--- app/src/main/java/com/example/sssmarketappclone/ItemListsFragment.kt
+++ app/src/main/java/com/example/sssmarketappclone/ItemListsFragment.kt
@@ -1,12 +1,20 @@
 package com.example.sssmarketappclone
 
+import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.media.AudioAttributes
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
+import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -63,6 +71,10 @@ class ItemListsFragment : Fragment() {
             addItemDecoration(DividerItemDecoration(mainActivity, LinearLayout.VERTICAL))
         }
 
+        binding.imageButton.setOnClickListener{
+            notification()
+        }
+
         spinner()
     }
     companion object {
@@ -82,6 +94,46 @@ class ItemListsFragment : Fragment() {
         val spinnerAdapter = ArrayAdapter(mainActivity,android.R.layout.simple_spinner_item, spinnerDatas)
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = spinnerAdapter
+    }
+
+    private fun notification() {
+        val manager = mainActivity.getSystemService(Activity.NOTIFICATION_SERVICE) as NotificationManager
+        val builder: NotificationCompat.Builder
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelID = "myChannel16"
+            val channelName = "My Channel 16"
+            val channel = NotificationChannel(
+                channelID,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "Ch.16"
+                setShowBadge(true)
+                val uri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE) // type notification 대신
+                val audioAttributes = AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .build()
+                setSound(uri, audioAttributes)
+                enableVibration(false)  // 진동 Off(진동싫어)
+            }
+            manager.createNotificationChannel(channel)
+            builder = NotificationCompat.Builder(mainActivity, channelID)
+        } else {
+            builder = NotificationCompat.Builder(mainActivity)
+        }
+
+        builder.run {
+            setSmallIcon(R.drawable.notify_resize_01)
+            setWhen(System.currentTimeMillis())
+            setContentTitle(resources.getString(R.string.app_name))
+            setContentText("새 알림 도착")
+            setStyle(
+                NotificationCompat.BigTextStyle().bigText("새로운 알림이 도착했습니다!!!")
+            )
+        }
+        manager.notify(16,builder.build())
     }
 
     override fun onDestroyView() {
